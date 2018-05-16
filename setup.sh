f4b25187b1249a39108f385e47251b50aa6d819a
JOAO EMILIO SILVA <joaoemilio@MacBook-Joao.local>
2018-05-16 15:40:55 -0300

    mariadb updates

--- setup.sh
+++ setup.sh
@@ -2,17 +2,22 @@
 
 VERSION=""
 DB=""
+PRODUCT=""
+
+PRODUCT=$1
 
 #if [ $1 = "2.2.0" ]
 #then
-   VERSION=$1
+   VERSION=$2
 #fi
 
 #if [ $2 = "mariadb" ]
 #then
-   DB=$2
+   DB=$3
 #fi
+
+
 
 cp -av resources /tmp/
 cd /tmp
-sh ./resources/setup-apim-$VERSION-$DB.sh
+sh ./resources/setup-$PRODUCT-$VERSION-$DB.sh
