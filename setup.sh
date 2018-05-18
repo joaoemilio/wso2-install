8fe85a033e667cd29aac1e2c828100bac2ada931
JOAO EMILIO SILVA <joaoemilio@MacBook-Joao.local>
2018-05-17 21:20:59 -0300

    mariadb updates

--- setup.sh
+++ setup.sh
@@ -21,3 +21,11 @@ PRODUCT=$1
 cp -av resources /tmp/
 cd /tmp
 sh ./resources/setup-$PRODUCT-$VERSION-$DB.sh
+
+SSL_CN="apim21.minegames.com.br"
+#openssl genrsa -des3 -out $SSL_CN.key 1024
+#sudo openssl req -new -key $SSL_CN.key -out $SSL_CN.csr
+#sudo cp $SSL_CN.key $SSL_CN.key.org
+#sudo openssl rsa -in $SSL_CN.key.org -out $SSL_CN.key
+#sudo openssl x509 -req -days 365 -in $SSL_CN.csr -signkey $SSL_CN.key -out $SSL_CN.crt
+#sudo cp identity* /etc/nginx/ssl/
