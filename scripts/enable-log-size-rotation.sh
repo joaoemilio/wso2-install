2ce7d5d5a25d2999915d94d4ca11cddef9f78feb
Joao Emilio <joaoemilio@gmail.com>
2018-06-01 14:39:16 -0300

    pattern #3

--- scripts/enable-log-size-rotation.sh
+++ scripts/enable-log-size-rotation.sh
@@ -4,14 +4,14 @@ function printHelp() {
     echo "sh enable-log-size-rotation.sh <script com variaveis de ambiente> <apim|is|is-as-km|am-analytics|is-analytics> <2.2.0|5.5.0>"
 }
 
-# chamar o script de variaveis do ambiente do cliente
-source $1
+RESROUCES_HOME="/opt/wso2/install/wso2-install/resources/"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# carregar variaveis de ambiente e functions utilitarias
+source $1
 source $DIR/functions.sh
-bla 
 
 getCarbonHomeByProduct
-echo "depois de chamar"
 CARBON_HOME=`getCarbonHomeByProduct $2`
 PRODUCT=$2
 VERSION=$3
@@ -32,4 +32,28 @@ if [ -z "${CARBON_HOME}" ]; then
     echo "A variavel CARBON_HOME não foi configurada corretamente"
     printHelp 
     exit 
+fi
+
+if [ -z "${LOG_ROTATION_TYPE}" ]; then
+    echo "A variavel LOG_ROTATION_TYPE não foi configurada. Utilizando default 'daily'"
+    LOG_ROTATION_TYPE="daily"
+elif 
+    if [ -z "${CARBON_LOGFILE_MAXFILESIZE}" ]; then
+        echo "A variavel CARBON_LOGFILE_MAXFILESIZE não foi configurada corretamente"
+        exit
+    fi 
+    if [ -z "${CARBON_LOGFILE_MAXBACKUPINDEX}" ]; then
+        echo "A variavel CARBON_LOGFILE_MAXBACKUPINDEX não foi configurada corretamente"
+        exit
+    fi 
+fi
+
+cp -v $CARBON_HOME/repository/conf/log4j.properties  $CARBON_HOME/repository/conf/log4j.properties.orig
+
+FILE=$RESOURCES_HOME/$PRODUCT/$VERSION/conf/log4j-$LOG_ROTATION_TYPE-rotation.properties
+cp -v $FILE $CARBON_HOME/repository/conf/log4j.properties
+
+if [ "$LOG_ROTATION_TYPE" = "size" ]; then
+    sed -i "s,{{CARBON_LOGFILE_MAXFILESIZE}},$CARBON_LOGFILE_MAXFILESIZE,g" $FILE
+    sed -i "s,{{CARBON_LOGFILE_MAXBACKUPINDEX}},$CARBON_LOGFILE_MAXBACKUPINDEX,g" $FILE
 fi
