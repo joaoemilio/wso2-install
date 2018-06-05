ba63c4bc4a61e06fbec3d83ee9273b1747aafb91
Joao Emilio <joaoemilio@gmail.com>
2018-06-05 19:28:46 -0300

    refactoring

--- scripts/wso2/is-as-km/5.5.0/env-vars.sh
+++ scripts/wso2/is-as-km/5.5.0/env-vars.sh
@@ -8,6 +8,6 @@ RESOURCES_HOME="/tmp/resources"
 _DATAHORA=`date +"%y%m%d_%H%M%S"`
 # Essa variável contém o binário (.zip) que vai ser utilizado na instalação
 LATEST=`ls -ltr ~/.wum-wso2/products/wso2is-km/5.5.0/ | tail -n1 | awk '{print $NF}'`
-WSO2IS_INSTALL_PATH="/home/wso2/.wum-wso2/products/wso2is/5.5.0/$LATEST"
+WSO2IS_INSTALL_PATH="/home/wso2/.wum-wso2/products/wso2is-km/5.5.0/$LATEST"
 echo "$WSO2AM_INSTALL_PATH"
 WSO2_INSTALL_PATH="/opt/wso2/install/wso2-install"
