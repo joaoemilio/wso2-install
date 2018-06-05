a01399ad2933cce62975a10cf91b3e8a2b2e845e
Joao Emilio <joaoemilio@gmail.com>
2018-06-05 10:39:11 -0300

    refactoring

--- scripts/wso2/apim/2.2.0/install.sh
+++ scripts/wso2/apim/2.2.0/install.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function install_apim() {
     echo "configurando variaveis de ambiente padrão para o wso2 api manager 2.2.0"
-    $DIR/env-vars.sh
+    source $DIR/env-vars.sh
 
     echo "configurando variaveis de ambiente do cliente: $1"
     source $1
@@ -15,7 +15,7 @@ function install_apim() {
     echo "# removendo instalação anterior"
     mv $APIM_HOME $CARBON_HOME.$_DATAHORA
 
-    echo "# descompactando binario de instalacao"
+    echo "# descompactando binario de instalacao: $WSO2AM_INSTALL_PATH"
     cd /opt/wso2/
     unzip -q $WSO2AM_INSTALL_PATH
 
