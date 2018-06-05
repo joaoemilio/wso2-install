85140adf4b6c16e4022cb6b1b9d27a8a80ecb76f
Joao Emilio <joaoemilio@gmail.com>
2018-06-05 18:44:21 -0300

    refactoring

--- scripts/wso2/apim/2.2.0/gateway.sh
+++ scripts/wso2/apim/2.2.0/gateway.sh
@@ -6,6 +6,10 @@ function backup() {
     cp $CARBON_HOME/repository/conf/carbon.xml $CARBON_HOME/repository/conf/carbon.xml.orig.$_DATAHORA
     cp $CARBON_HOME/repository/conf/registry.xml $CARBON_HOME/repository/conf/registry.xml.orig.$_DATAHORA
     cp $CARBON_HOME/repository/conf/api-manager.xml $CARBON_HOME/repository/conf/api-manager.xml.orig.$_DATAHORA
+
+    cp $CARBON_HOME/repository/conf/axis2/axis2_client.xml $CARBON_HOME/repository/conf/axis2/axis2_client.xml.$_DATAHORA
+    cp $CARBON_HOME/repository/conf/nhttp.properties $CARBON_HOME/repository/conf/nhttp.properties.$_DATAHORA
+    cp $CARBON_HOME/repository/conf/passthru-http.properties  $CARBON_HOME/repository/conf/passthru-http.properties.$_DATAHORA
 }
 
 function setup() {
@@ -14,7 +18,11 @@ function setup() {
     echo "substituindo arquivos de configuracao"
     cp $RESOURCES_HOME/$PRODUCT/$VERSION/conf/carbon-gw.xml $CARBON_HOME/repository/conf/carbon.xml
     cp $RESOURCES_HOME/$PRODUCT/$VERSION/conf/registry-gw.xml $CARBON_HOME/repository/conf/registry.xml
-    cp $RESOURCES_HOME/$PRODUCT/$VERSION/conf/api-manager-gw.xml $CARBON_HOME/repository/conf/api-manager.xml  
+    cp $RESOURCES_HOME/$PRODUCT/$VERSION/conf/api-manager-gw.xml $CARBON_HOME/repository/conf/api-manager.xml
+    cp $RESOURCES_HOME/$PRODUCT/$VERSION/conf/axis2/axis2_client.xml $CARBON_HOME/repository/conf/axis2/axis2_client.xml
+    cp $RESOURCES_HOME/$PRODUCT/$VERSION/conf/nhttp.properties $CARBON_HOME/repository/conf/nhttp.properties
+    cp $RESOURCES_HOME/$PRODUCT/$VERSION/conf/passthru-http.properties  $CARBON_HOME/repository/conf/passthru-http.properties
+
 }
 
 function cleanup() {
