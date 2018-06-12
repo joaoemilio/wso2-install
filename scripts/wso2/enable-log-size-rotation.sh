4c5835fbf970d042110a2cded195720290d83231
Joao Emilio <joaoemilio@gmail.com>
2018-06-12 19:33:56 -0300

    refactoring

--- scripts/wso2/enable-log-size-rotation.sh
+++ scripts/wso2/enable-log-size-rotation.sh
@@ -9,7 +9,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # carregar variaveis de ambiente e functions utilitarias
 source $1
-source $DIR/functions.sh
+source $DIR/../functions.sh
 
 setCarbonHomeByProduct $2
 PRODUCT=$2
