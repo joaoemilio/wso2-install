3d1a615aada1145b33c64d40c23d70bcd39192ae
Joao Emilio <joaoemilio@gmail.com>
2018-06-13 10:52:12 -0300

    refactoring

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -50,7 +50,7 @@ function replaceVars( ) {
         for i in "${_VARS[@]}"
         do
 	        echo "substituir $i em $FILE" >> /opt/wso2/install/wso2-install/debug.log
-            _VALUE="echo $`echo $i`"
+            _VALUE="${!i}"
             echo "$_VALUE" >> /opt/wso2/install/wso2-install/debug.log
             sed -i "s,{{$i}},$_VALUE,g" $FILE
         done
