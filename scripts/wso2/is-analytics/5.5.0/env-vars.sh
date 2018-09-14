8f1f503da2eed092bb3074093c48cf623d14708d
Joao Emilio <joaoemilio@gmail.com>
2018-09-14 11:41:55 -0300

    atualizacao do wum3 para recuperar o ultimo pacote full do wso2apim 2.2.0

--- scripts/wso2/is-analytics/5.5.0/env-vars.sh
+++ scripts/wso2/is-analytics/5.5.0/env-vars.sh
@@ -5,7 +5,7 @@ VERSION="5.5.0"
 RESOURCES_HOME="/tmp/resources"
 _DATAHORA=`date +"%y%m%d_%H%M%S"`
 # Essa variável contém o binário (.zip) que vai ser utilizado na instalação
-LATEST=`ls -ltr ~/.wum-wso2/products/wso2is-analytics/5.5.0/ | tail -n1 | awk '{print $NF}'`
-WSO2AM_INSTALL_PATH="/home/wso2/.wum-wso2/products/wso2is-analytics/5.5.0/$LATEST"
+LATEST=`ls -ltr ~/.wum3/products/wso2is-analytics/5.5.0/full/ | tail -n1 | awk '{print $NF}'`
+WSO2AM_INSTALL_PATH="/home/wso2/.wum3/products/wso2is-analytics/5.5.0/full/$LATEST"
 echo "$WSO2AM_INSTALL_PATH"
 WSO2_INSTALL_PATH="/opt/wso2/install/wso2-install"
