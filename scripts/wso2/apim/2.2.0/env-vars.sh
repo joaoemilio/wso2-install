33f5c67c70d1a4cef38e94d6c0debd3e5e986137
Joao Emilio <joaoemilio@gmail.com>
2018-09-14 10:03:10 -0300

    atualizacao do wum3 para recuperar o ultimo pacote full do wso2apim 2.2.0

--- scripts/wso2/apim/2.2.0/env-vars.sh
+++ scripts/wso2/apim/2.2.0/env-vars.sh
@@ -9,6 +9,6 @@ _DATAHORA=`date +"%y%m%d_%H%M%S"`
 # Essa variável contém o binário (.zip) que vai ser utilizado na instalação
 WUM_HOME="/home/wso2/.wum3"
 LATEST=`ls -ltr $WUM_HOME/products/wso2am/2.2.0/full/ | tail -n1 | awk '{print $NF}'`
-WSO2AM_INSTALL_PATH="$WUM_HOME/products/wso2am/2.2.0/$LATEST"
+WSO2AM_INSTALL_PATH="$WUM_HOME/products/wso2am/2.2.0/full/$LATEST"
 echo "$WSO2AM_INSTALL_PATH"
 WSO2_INSTALL_PATH="/opt/wso2/install/wso2-install"
