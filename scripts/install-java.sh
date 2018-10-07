766bcf173b0018f82b15cdcf94f890a62ac88d04
Joao Emilio <joaoemilio@gmail.com>
2018-10-07 12:35:37 -0300

    mudanca no site da oracle para baixar jdk

--- scripts/install-java.sh
+++ scripts/install-java.sh
@@ -49,64 +49,31 @@ fi
 
 
 # set base download location
-URL="http://www.oracle.com"
-DOWNLOAD_URL1="${URL}/technetwork/java/javase/downloads/index.html"
-DOWNLOAD_URL2=$(curl -s $DOWNLOAD_URL1 | egrep -o "\/technetwork\/java/\javase\/downloads\/${JAVA_TYPE}${JAVA_VERSION}-downloads-.*\.html" | head -1)
+#URL="https://www.oracle.com"
+#DOWNLOAD_URL1="${URL}/technetwork/java/javase/downloads/index.html"
+#DOWNLOAD_URL2=$(curl -s $DOWNLOAD_URL1 | egrep -o "\/technetwork\/java/\javase\/downloads\/jdk8-downloads-.*\.html" | head -1)
 
 # check to make sure we got to oracle
-if [[ -z "$DOWNLOAD_URL2" ]]; then
-  echo "Could not to oracle - $DOWNLOAD_URL1"
-  exit 1
-fi
+#if [[ -z "$DOWNLOAD_URL2" ]]; then
+#  echo "Could not to oracle - $DOWNLOAD_URL1"
+#  exit 1
+#fi
 
 # set download url
-DOWNLOAD_URL3="$(echo ${URL}${DOWNLOAD_URL2}|awk -F\" {'print $1'})"
-DOWNLOAD_URL4=$(curl -s "$DOWNLOAD_URL3" | egrep -o "http\:\/\/download.oracle\.com\/otn-pub\/java\/jdk\/[7-8]u[0-9]+\-(.*)+\/${JAVA_TYPE}-[7-8]u[0-9]+(.*)linux-x64.${EXT}"|tail -n1)
-
+#DOWNLOAD_URL3="$(echo ${URL}${DOWNLOAD_URL2}|awk -F\" {'print $1'})"
+#echo "URL3" $DOWNLOAD_URL3
+#DOWNLOAD_URL4=$(curl -s "$DOWNLOAD_URL3" | egrep -o "https\:\/\/download.oracle\.com\/otn-pub\/java\/jdk\/[7-8]u[0-9]+\-(.*)+\/${JAVA_TYPE}-[7-8]u[0-9]+(.*)linux-x64.${EXT}"|tail -n1)
+#echo "URL4" $DOWNLOAD_URL4
 # check to make sure url exists
-if [[ -z "$DOWNLOAD_URL4" ]]; then
-  echo "Could not get ${JAVA_TYPE} download url - $DOWNLOAD_URL4"
-  exit 1
-fi
+#if [[ -z "$DOWNLOAD_URL4" ]]; then
+#  echo "Could not get ${JAVA_TYPE} download url - $DOWNLOAD_URL4"
+#  exit 1
+#fi
 # set download file name
+DOWNLOAD_URL4="http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.tar.gz"
 JAVA_INSTALL=$(basename $DOWNLOAD_URL4)
 
-if [[ "$EXT" == "rpm" ]]; then
-
-        # download java
-        echo -e "\n\e[32mDownloading\e[0m: $DOWNLOAD_URL4"
-        while true;
-        do echo -n .;sleep 1;done &
-        cd /tmp; wget -q --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" $DOWNLOAD_URL4 -O $JAVA_INSTALL > /dev/null 2>&1
-        kill $!; trap 'kill $!' SIGTERM;
-        # install rpm
-        echo -e "\n\e[32mInstalling\e[0m: $JAVA_INSTALL\r"
-        while true;
-        do echo -n .;sleep 1;done &
-        rpm -Uvh /tmp/$JAVA_INSTALL > /dev/null 2>&1
-        kill $!; trap 'kill $!' SIGTERM;
-        echo -e "\n\e[32mInstall\e[0m Complete\n"
-        # get dirname
-        JAVA_DIR=$(ls -tr /usr/java/|grep ${JAVA_TYPE}|head -n 1)
-        # set temp env var
-        export JAVA_HOME=/usr/java/${JAVA_DIR}
-        # set perm env var
-        echo "export JAVA_HOME=/usr/java/${JAVA_DIR}" >> /etc/environment
-        # set if jdk is used
-        if [[ "$JAVA_TYPE" = "jdk" ]]; then
-                # set temp env var
-                export JRE_HOME=/usr/java/${JAVA_DIR}/jre
-                # set perm env var
-                echo "export JRE_HOME=/usr/java/${JAVA_DIR}/${JAVA_TYPE}" >> /etc/environment
-        fi
-        # make sure java installed
-        ls /usr/java/${JAVA_DIR} > /dev/null 2>&1
-        if [[ "$?" != 0  ]]; then
-                echo -e "\n\e[31mError\e[0m: Java does not seem to be installed correctly,\nPlease try again or email admin: ${ADMIN_EMAIL}\n"
-                exit 1
-        fi
-
-elif [[ "$EXT" == "tar" || "$EXT" == "tar.gz" ]]; then
+if [[ "$EXT" == "tar" || "$EXT" == "tar.gz" ]]; then
 
         # download java
         echo -e "\n\e[32mDownloading\e[0m: $DOWNLOAD_URL4"
